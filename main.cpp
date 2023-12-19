101814bceb4c0fdaedbf01db7095ebf6beb4ba8f
Yuh-Hypnotized <142199381+Yuh-Hypnotized@users.noreply.github.com>
2023-12-19 13:10:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <Windows.h>
 #include <iomanip>
-#include "Matrix.h"
+#include <vector>
+#include "Matrix.h"  //Include Matrix class definitions
 using namespace std;
 
 int main() {    //main function starts here.
-	int manipulationJudge = -1;
-	bool flag = true;
+	int manipulationJudge = -1;    //Initialize judge variable of manipulation
+	bool flag = true;    //Flags user understood or not
 	cout << "====================\n"
 		<< "Read before use:\n"
 		<< "This program is only capable for matrix smaller than 20*20.\n"
 		<< "Follow the instructions when using the calculator.\n"
 		<< "====================\n"
 		<< "Understand? (type 'Y' for yes):";
-	char uds = cin.get();
+	char uds = cin.get();   //understand
 	if (uds == 'Y') {
 		cout << "\nGet started!\n\n" << endl;
 	}
-	else {
+	else {    //If not understand, quit the program.
 		flag = false;
 		cout << "\n\nProgram exited due to disagreement.\n\n" << endl;
 		Sleep(1000);
 	}
-	while (flag == true) {
-		cout << "====================\n" << "1: Transpose(转置)" << "\n2: Addition(加法)" << "\n3: Subtraction(减法)"
-			<< "\n4: Multiplication(乘法)" << "\n5: Inverse(求逆矩阵)" << "\n6: Determinant(求行列式)" 
-			<< "\n7: Adjugate(求伴随矩阵)" << "\n-1: Exit" << endl;
+	while (flag == true) {   //Loop until manipulationJudge = -1
+		cout << "====================\n" << "1: Transpose(转置)" << "\n2: Row-Minimalization(化为行最简形)" 
+			<< "\n3: Addition(加法)" << "\n4: Subtraction(减法)"
+			<< "\n5: Multiplication(乘法)" << "\n6: Inversion(求逆矩阵)" << "\n7: Determinant(求行列式)" 
+			<< "\n8: Adjugate(求伴随矩阵)" << "\n9: Solve Linear Equations(求解线性方程组)" 
+			<< "\n10: Eigenvalues & Eigenvectors(求特征值和特征向量)" << "\n-1: Exit" << endl;
 		cout << "\nPlease select your manipulation: ";
 		cin >> manipulationJudge;
-		if (manipulationJudge == -1) {
+		if (manipulationJudge == -1) {   //Exit
 			flag = false;
 			cout << "\n\n====================\n" << "Program exiting in 5 seconds...\n"
 				<< "If you met any problem or you have some suggestions\n"
@@ -38,7 +41,8 @@ int main() {    //main function starts here.
 			Sleep(500);
 			break;
 		}
-		else if (manipulationJudge == 1) {
+
+		else if (manipulationJudge == 1) {   //Transpose
 			int row, column;
 			cout << "How many rows? Enter: "; cin >> row;
 			cout << "How many columns? Enter: "; cin >> column;
@@ -53,21 +57,37 @@ int main() {    //main function starts here.
 			cout << "\nThe result is: " << endl;
 			myMatrix.printMatrix();
 		}
-		else if (manipulationJudge == 2) {
+
+		else if (manipulationJudge == 2) {    //row-minimalization
 			int row, column;
-			double matrix1[20][20], matrix2[20][20];
+			cout << "How many rows? Enter: "; cin >> row;
+			cout << "How many columns? Enter: "; cin >> column;
+			cout << "\nEnter your matrix: " << endl;
+			Matrix myMatrix(row, column);
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < column; j++)
+					cin >> myMatrix.matrixA[i][j];
+			}
+			myMatrix.rowMinimalize();
+			cout << "\nThe result is: " << endl;
+			myMatrix.printMatrix();
+		}
+
+		else if (manipulationJudge == 3) {   //Addition
+			int row, column;
+			array<array<double, 20>, 20> matrix1, matrix2;
 			cout << "How many rows? Enter: "; cin >> row;
 			cout << "How many columns? Enter: "; cin >> column;
 			Matrix myMatrix(row, column);
 			cout << "\nEnter your first matrix: " << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {     //Input matrix1
 				for (int j = 0; j < column; j++) {
 					cin >> matrix1[i][j];
 				}
 			}
 			cout << "Enter your second matrix: \n"
 				<< "<ATTENTION>Your second matrix MUST have the same size as the first one." << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {     //Input matrix2
 				for (int j = 0; j < column; j++) {
 					cin >> matrix2[i][j];
 				}
@@ -76,21 +96,22 @@ int main() {    //main function starts here.
 			cout << "\nThe result is: " << endl;
 			myMatrix.printMatrix();
 		}
-		else if (manipulationJudge == 3) {
+
+		else if (manipulationJudge == 4) {    //Subtraction
 			int row, column;
-			double matrix1[20][20], matrix2[20][20];
+			array<array<double, 20>, 20> matrix1, matrix2;
 			cout << "How many rows? Enter: "; cin >> row;
 			cout << "How many columns? Enter: "; cin >> column;
 			Matrix myMatrix(row, column);
 			cout << "\nEnter your first matrix: " << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {    //Input matrix1
 				for (int j = 0; j < column; j++) {
 					cin >> matrix1[i][j];
 				}
 			}
 			cout << "Enter your second matrix: \n"
 				<< "<ATTENTION>Your second matrix MUST have the same size as the first one." << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {    //Input matrix2
 				for (int j = 0; j < column; j++) {
 					cin >> matrix2[i][j];
 				}
@@ -99,23 +120,25 @@ int main() {    //main function starts here.
 			cout << "\nThe result is: " << endl;
 			myMatrix.printMatrix();
 		}
-		else if (manipulationJudge == 4) {
+
+		else if (manipulationJudge == 5) {    //Multiplication
 			int row, column1, column2;
-			double matrix1[20][20], matrix2[20][20];
+			array<array<double, 20>, 20> matrix1, matrix2;
 			cout << "How many rows in the first matrix? Enter: "; cin >> row;
 			cout << "How many columns in the first matrix? Enter: "; cin >> column1;
+			//The left matrix's column should be equal to the right matrix's row
 			cout << "So there are " << column1 << " rows in the second matrix." << endl;
 			cout << "How many columns in the second matrix? Enter: "; cin >> column2;
 			Matrix myMatrix(row, column2);
 			cout << "\nEnter your first matrix: " << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {    //Input matrix1
 				for (int j = 0; j < column1; j++) {
 					cin >> matrix1[i][j];
 				}
 			}
 			cout << "Enter your second matrix: \n"
 				<< "<ATTENTION>Your second matrix MUST have the same rows as the first one has in columns." << endl;
-			for (int i = 0; i < column1; i++) {
+			for (int i = 0; i < column1; i++) {    //Input matrix2
 				for (int j = 0; j < column2; j++) {
 					cin >> matrix2[i][j];
 				}
@@ -124,41 +147,51 @@ int main() {    //main function starts here.
 			cout << "\nThe result is: " << endl;
 			myMatrix.printMatrix();
 		}
-		else if (manipulationJudge == 5) {
+
+		else if (manipulationJudge == 6) {    //Inversion
 			int row;
 			cout << "How many rows? Enter: "; cin >> row;
 			Matrix myMatrix(row, row);
+			//Only n * n matrix can have inverse matrix
 			cout << "So there are " << row << " columns in the matrix." << endl;
 			cout << "\nEnter your matrix: " << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {    //Input matrix
 				for (int j = 0; j < row; j++) {
 					cin >> myMatrix.matrixA[i][j];
 				}
 			}
-			myMatrix.inverseMatrix();
-			cout << "\nThe result is: " << endl;
-			myMatrix.printMatrix();
+			//Double-check matrix determinant to clarify whether it is inversible
+			if (myMatrix.matrixDeterminant() == 0)   //Not inversible
+				cout << "Matrix not inversible!\n" << endl;
+			else {      //Inversible
+				myMatrix.inverseMatrix();
+				cout << "\nThe result is: " << endl;
+				myMatrix.printMatrix();
+			}
 		}
-		else if (manipulationJudge == 6) {
+
+		else if (manipulationJudge == 7) {   //Determinant
 			int row;
 			cout << "How many rows? Enter: "; cin >> row;
 			Matrix myMatrix(row, row);
+			//Only n * n matrix can calculate determinant
 			cout << "So there are " << row << " columns in the matrix." << endl;
 			cout << "\nEnter your matrix: " << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {    //Input matrix
 				for (int j = 0; j < row; j++) {
 					cin >> myMatrix.matrixA[i][j];
 				}
 			}
 			cout << "\nThe result is: " << myMatrix.matrixDeterminant() << endl;
 		}
-		else if (manipulationJudge == 7) {
+
+		else if (manipulationJudge == 8) {   //Adjugate
 			int row;
 			cout << "How many rows? Enter: "; cin >> row;
 			Matrix myMatrix(row, row);
 			cout << "So there are " << row << " columns in the matrix." << endl;
 			cout << "\nEnter your matrix: " << endl;
-			for (int i = 0; i < row; i++) {
+			for (int i = 0; i < row; i++) {    //Input matrix
 				for (int j = 0; j < row; j++) {
 					cin >> myMatrix.matrixA[i][j];
 				}
@@ -167,57 +200,103 @@ int main() {    //main function starts here.
 			cout << "\nThe result is: " << endl;
 			myMatrix.printMatrix();
 		}
-		else {
+
+		else if (manipulationJudge == 9) {    //Linear system solutions
+			int row;
+			cout << "How many x's? Enter: "; cin >> row;
+			vector<double> const_vec;
+			const_vec.resize(row);
+			Matrix myMatrix(row, row);
+			cout << "Tips:" << endl;
+			cout << "For example, if your equations are like this:" << endl;
+			cout << "a11x1 + a12x2 + a13x3 = b1" << endl;
+			cout << "a21x1 + a22x2 + a23x3 = b2" << endl;
+			cout << "a31x1 + a32x2 + a33x3 = b3\n" << endl;
+			cout << "Then you should enter:" << endl;
+			cout << "a11 a12 a13 b1" << endl;
+			cout << "a21 a22 a23 b2" << endl;
+			cout << "a31 a32 a33 b3" << endl;
+			cout << "\nEnter your augmented matrix: " << endl;
+			for (int i = 0; i < row; i++) {     //Input augmented matrix
+				for (int j = 0; j <= row; j++) {
+					if (j < row) cin >> myMatrix.matrixA[i][j];
+					else cin >> const_vec[i];
+				}
+			}
+			int solutionType;   //0 for unsolvable; 1 for single solution; 2 for Ax=0 type; 3 for Ax=b but r(A)<n type
+			any solutions = myMatrix.linearSolution(const_vec, solutionType);
+			
+			if (solutionType == 0)      //0 (unsolvable)
+				cout << "\nThis linear system is unsolvable!\n" << endl;
+			else if (solutionType == 1) {      //1 (single solution based on Cramer's rule)
+				cout << "\nThe Solution is: " << endl;
+				int count = 1;
+				vector<double> solution = any_cast<vector<double>>(solutions);
+				for (size_t i = 0; i < solution.size(); i++) {
+					cout << "x" << count << " = " << solution[i] << endl;
+					count++;
+				}
+				cout << endl;
+			}
+			else if (solutionType == 2) {    //2 (Ax=0 type) (inf)
+				cout << "\nThe Basic Solutions are: " << endl;
+				vector<vector<double>> solution = any_cast<vector<vector<double>>>(solutions);
+				int count = 1;
+				for (auto item : solution) {   //output basic solutions
+					cout << "e" << count << " = (";
+					for (auto element : item)
+						cout << setw(8) << " " << element;
+					cout << ");" << endl;
+					count++;
+				}
+				cout << "So the general solution is: " << endl;   //general solution
+				for (int i = 1; i < count - 1; i++)
+					cout << "k" << i << "*e" << i << " + ";
+				cout << "k" << count - 1 << "*e" << count - 1 << endl;
+				cout << endl;
+			}
+			else {    //3 (Ax=b but r(A)<n type) (inf)
+				cout << "\nThe Basic Solutions are: " << endl;
+				vector<vector<double>> solution = any_cast<vector<vector<double>>>(solutions);
+				vector<double> particular = solution[solution.size() - 1];
+				solution.pop_back();
+				int count = 1;
+				for (auto item : solution) {
+					cout << "e" << count << " = (";
+					for (auto element : item)
+						cout << setw(8) << " " << element;
+					cout << ");" << endl;
+					count++;
+				}
+				cout << "Particular solution: (";
+				for (auto element : particular)
+					cout << setw(8) << " " << element;
+				cout << ")" << endl;
+				cout << "So the general solution is: " << endl;   //general solution
+				for (int i = 1; i < count; i++)
+					cout << "k" << i << "*e" << i << " + ";
+				cout << "Particular solution" << endl;
+				cout << endl;
+			}
+		}
+
+		else if (manipulationJudge == 10) {
+			int row;
+			cout << "How many rows? Enter: "; cin >> row;
+			Matrix myMatrix(row, row);
+			cout << "So there are " << row << " columns in the matrix." << endl;
+			cout << "\nEnter your matrix: " << endl;
+			for (int i = 0; i < row; i++) {    //Input matrix
+				for (int j = 0; j < row; j++) {
+					cin >> myMatrix.matrixA[i][j];
+				}
+			}
+			myMatrix.eigenvector();   //call eigenvector function
+		}
+		else {    //Invalid manipulation input
 			cout << "\nInvalid input! Try again.\n" << endl;
 		}
 		Sleep(1000);
 	}
 	return 0;
 }
-
-/*	The LAGACY of inverse matrix codes in v1.0
-
-		double assistMatrix[20][20], resultMatrix[20][20];
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < column; j++) {
-				assistMatrix[i][j] = matrixA[i][j];
-			}
-		}
-		for (int i = 0; i < row; i++) {    //(A+E)model
-			for (int j = 0; j < row; j++) {
-				if (i == j)
-					assistMatrix[i][j + column] = 1;
-				else
-					assistMatrix[i][j + column] = 0;
-			}
-		}for (int i = 0; i < row; i++) {    //ri+=k*rj
-			if (assistMatrix[i][i] == 0) {   //check for unexpected ?/0 problem
-				int pin = i + 1;
-				while (assistMatrix[pin][i] == 0 and pin!=i) {
-					pin = (pin + 1) % row;
-				}
-				for (int k = 0; k < column + row; k++) {
-					assistMatrix[i][k] += assistMatrix[pin][k];
-				}
-			}
-			for (int j = 1; j < row; j++) {
-
-				double ratio = assistMatrix[(i + j) % row][i] / assistMatrix[i][i];
-				for (int k = 0; k < column + row; k++) {
-					assistMatrix[(i + j) % row][k] -= assistMatrix[i][k] * ratio;
-				}
-			}
-		}
-		for (int i = 0; i < row; i++) {    //k*ri
-			double ratio = assistMatrix[i][i];
-			for (int k = 0; k < column + row; k++) {
-				assistMatrix[i][k] /= ratio;
-			}
-		}
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < column; j++) {
-				resultMatrix[i][j] = assistMatrix[i][j + row];
-			}
-		}
-		setMatrixValue(resultMatrix, row, column);
-*/
