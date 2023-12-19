07bc0037df9089cb46b52b8be1ad96b49c728eda
Yuh-Hypnotized <142199381+Yuh-Hypnotized@users.noreply.github.com>
2023-12-19 13:06:46 +0800

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,17 +1,24 @@
+//Matrix class declarations
+#include <array>  //array included
+#include <vector>  //vector included
+#include <any>   //any included
 class Matrix {
 public:
 	int row, column;
-	double matrixA[20][20] = {};
+	std::array<std::array<double, 20>, 20> matrixA = {};
 	Matrix(int row_imput, int column_input);
-	void setMatrixValue(double matrix_input[20][20], int row_input, int column_input);
+	void setMatrixValue(std::array<std::array<double, 20>, 20> matrix_input, int row_input, int column_input);
 	void printMatrix() const;
 	void matrixTranspose();
-	void matrixAddition(double matrix_input1[20][20], double matrix_input2[20][20], int row_input, int column_input);
-	void matrixSubtraction(double matrix_input1[20][20], double matrix_input2[20][20], int row_input, int column_input);
-	void matrixMultiplication(double matrix_input1[20][20], double matrix_input2[20][20], int row_input1, int column_input1, int column_input2);
+	void matrixAddition(std::array<std::array<double, 20>, 20> matrix_input1, std::array<std::array<double, 20>, 20> matrix_input2, int row_input, int column_input);
+	void matrixSubtraction(std::array<std::array<double, 20>, 20> matrix_input1, std::array<std::array<double, 20>, 20> matrix_input2, int row_input, int column_input);
+	void matrixMultiplication(std::array<std::array<double, 20>, 20> matrix_input1, std::array<std::array<double, 20>, 20> matrix_input2, int row_input1, int column_input1, int column_input2);
 	void inverseMatrix();
 	double matrixDeterminant();
 	double cofactor(int a, int b);
 	void adjugateMatrix();
+	std::any linearSolution(std::vector<double> const_vec, int& solutionType);
+	std::vector<bool> rowMinimalize();
+	void eigenvector();
 private:
 };
